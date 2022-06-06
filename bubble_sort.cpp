d2e5b4e7aace4b9749cae9f5a93a488ea8907d1d
UhaiMong <uhaimarma2016@gmail.com>
2022-06-06 10:09:57 +0600

    cmm

---
+++ bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX_ARRAY 50
+main()
+{
+    int array[MAX_ARRAY],i,j,size,temp,a;
+    cout<<"Enter the size of array: "<<"\n";
+    cin>>size;
+    cout<<"Enter the elements of the array: "<<"\n";
+    for(a=0; a<size; a++)
+    {
+        cin>>array[a];
+    }
+    for ( i = 0; i < size-1; i++)
+    {
+        for (j = 0; j < size - 1 -i; j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                 array[j + 1] = temp;
+            }
+        }
+        
+    }
+    cout<<"Sorted array is :"<<"\n";
+    for (a = 0; a < size; a++)
+    {
+        cout<<" "<<array[a];
+    }
+    
+    
+}
